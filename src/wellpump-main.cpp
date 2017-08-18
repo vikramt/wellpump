76f160f042ab7dab748f199d632d0a719670e833
vikram <viktar@gmail.com>
2017-08-17 20:12:23 -0400

    working

--- src/wellpump-main.cpp
+++ src/wellpump-main.cpp
@@ -11,21 +11,53 @@ be used with analogWrite().
 #include <ESP8266WiFi.h>
 
 #define LED     D0        // Led in NodeMCU at pin GPIO16 (D0).
-
+// LED breathing definitions
 #define BRIGHT    350     //max led intensity (1-500)
 #define INHALE    2250    //Inhalation time in milliseconds.
 #define PULSE     INHALE*1000/BRIGHT
 #define REST      1000    //Rest Between Inhalations.
+
 // EmonLiteESP definitions needed
 #define ADC_BITS 10
 #define REFERENCE_VOLTAGE 3.3
 #define CURRENT_RATIO 30
+#define MAINS_VOLTAGE  230
+
+// MQTT server definitions
+#define mqtturl io.adafruit.com
+#define mqttusername viktaraf
+#define mqttkey aec055e3559542aa94fbb217e3e45840
+#define mqtttopic firstone
+
+// Number of samples each time you measure
+#define SAMPLES_X_MEASUREMENT   1000
+// Time between readings, this is not specific of the library but on this sketch
+#define MEASUREMENT_INTERVAL    10000
+
 
 // Instantiate the lib and set its callback
 EmonLiteESP power;
 unsigned int currentCallback() {
     return analogRead(A0);
 }
+
+void powerMonitorLoop() {
+
+    static unsigned long last_check = 0;
+
+    if ((millis() - last_check) > MEASUREMENT_INTERVAL) {
+
+        double current = power.getCurrent(SAMPLES_X_MEASUREMENT);
+
+        Serial.print(F("[ENERGY] Power now: "));
+        Serial.print(int(current * MAINS_VOLTAGE));
+        Serial.println(F("W"));
+
+        last_check = millis();
+
+    }
+  }
+
 //----- Setup function. ------------------------
 void setup() {
   power.initCurrent(currentCallback, ADC_BITS, REFERENCE_VOLTAGE, CURRENT_RATIO);
@@ -33,25 +65,10 @@ void setup() {
   Serial.begin(9600);
 }
 
+
 //----- Loop routine. --------------------------
 void loop() {
-  //ramp increasing intensity, Inhalation:
-  for (int i=1;i<BRIGHT;i++){
-    digitalWrite(LED, LOW);          // turn the LED on.
-    delayMicroseconds(i*10);         // wait
-    digitalWrite(LED, HIGH);         // turn the LED off.
-    delayMicroseconds(PULSE-i*10);   // wait
-    delay(0);                        //to prevent watchdog firing.
-  }
-  //ramp decreasing intensity, Exhalation (half time):
-  for (int i=BRIGHT-1;i>0;i--){
-    digitalWrite(LED, LOW);          // turn the LED on.
-    delayMicroseconds(i*10);          // wait
-    digitalWrite(LED, HIGH);         // turn the LED off.
-    delayMicroseconds(PULSE-i*10);  // wait
-    i--;
-    delay(0);                        //to prevent watchdog firing.
-  }
-  delay(REST);
+  powerMonitorLoop();
+  delay(1);
 
 }
