5a5e413d7006f77b411c4e55c53a452668d1fd19
viktar <viktar@gmail.com>
2017-08-25 16:10:09 -0400

    alpha-0.0 untested

--- src/wellpump-main.cpp
+++ src/wellpump-main.cpp
@@ -31,8 +31,8 @@ be used with analogWrite().
 
 // Number of samples each time you measure
 #define SAMPLES_X_MEASUREMENT   1000
-// Time between readings, makign this under 1second main loop is about 1 sec
-#define MEASUREMENT_INTERVAL    800
+// Time between power readings, makign this under 1second main loop is about 1 sec
+#define MEASUREMENT_INTERVAL    700
 
 //wifi and mqtt connections
 //const char* ssid = "toods";
@@ -41,9 +41,11 @@ const char* ssid = "Tree";
 const char* password = "44445555";
 //const char* mqtt_server = "mqtt.thingspeak.com";
 const char* mqtt_server = "10.1.1.4";
+const char* mqtt_user = "wellpump";
+const char* mqtt_password = "unset";
 const char* mqtt_ch_current = "Sensor/wellsensor/current";
-const char* mqtt_ch_temp = "Sensor/wellsensor/temperature";
-const char* mqtt_ch_humi = "Sensor/wellsensor/humidity";
+const char* mqtt_ch_temp_humi = "Sensor/wellsensor/temp_humi";
+
 
 
 //Instantiate the wifi client and mqtt client
@@ -124,7 +126,7 @@ void reconnect() {
     String clientId = "WellSensor-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect("WellSensor-1","wellpump","unset")) {
+    if (client.connect("WellSensor-1",mqtt_user,mqtt_password)) {
       Serial.println("connected");
       // Once connected, publish an announcement...
       client.publish(mqtt_server, "connected");
@@ -161,14 +163,15 @@ void powerMonitorLoop() {
   }
 
 void get_temperature_humidity() {
-  ;
+  temperature=12;
+  humidity=30;
 }
 
 //----- Setup function. ------------------------
 void setup() {
 
   int i = 0; // counter for wasting readings
-  int j = 100 ; // how many to waste
+  int j = 70 ; // how many to waste
   power.initCurrent(currentCallback, ADC_BITS, REFERENCE_VOLTAGE, CURRENT_RATIO);
   pinMode(LED, OUTPUT);   // LED pin as output.
   Serial.begin(9600);
@@ -191,28 +194,59 @@ void setup() {
 void loop() {
 
   // This is for counting the loops in 1 sec intervals
-  static unsigned long loopcounter=0;
+  static unsigned int loopcounter=0;
+  static unsigned int wakeup=1;
+  static unsigned int publish_current=0;
+  static unsigned long current_on=0;
+  static unsigned long max_current_ma=0;
 
+  //read temperature and humidity every loop in global vars
+  get_temperature_humidity();
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
+  //read power and convert I into milliamps cast into integer every loop
   powerMonitorLoop();
-  //convert I into milliamps cast into integer
   milliamps=I*1000;
-
-  long now = millis();
-  if (now - lastMsg > MEASUREMENT_INTERVAL) {
-    lastMsg = now;
-
-
-    snprintf (msg, 75, "%ld", milliamps);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish(mqtt_ch_current, msg);
+  //count how long current is on if its greater than 2 amps and what the max is
+  if ( milliamps > 2000 ){
+    current_on++; // current on time in seconds
+    if ( milliamps > max_current_ma ) {
+      max_current_ma = milliamps;
+    }
+  } else {
+    publish_current=1;
   }
-  delay(1);
+
+  if( loopcounter >300  ) {
+    wakeup=1;
+    loopcounter=0;
+  }
+
+  if (  wakeup  || publish_current ) {
+
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+
+    if ( publish_current ) {
+      snprintf (msg, 75, "On:%ld Maxma:%ld", current_on,max_current_ma);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish(mqtt_ch_current, msg);
+      publish_current=0;
+      max_current_ma=0;
+    }
+    if ( wakeup ) {
+
+      snprintf (msg, 75, "T:%ld H:%ld", temperature,humidity);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish(mqtt_ch_temp_humi, msg);
+      wakeup=0;
+      temperature=0;humidity=0;
+    }
+  }
+  //delay 900 since the powerloop will take 100ms
+  delay(900);
 
 }
