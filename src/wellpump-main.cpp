54ed7f2bdad1a694a6bf0b3f651017007ee34f54
vikram <viktar@gmail.com>
2017-08-27 01:11:45 -0400

    Added wifi manager lib only as yet

--- src/wellpump-main.cpp
+++ src/wellpump-main.cpp
@@ -9,6 +9,9 @@ be used with analogWrite().
 #include <EmonLiteESP.h>
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <EEPROM.h>
 #include <SimpleDHT.h>
 
